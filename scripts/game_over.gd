ce5af96d232f3079b1a7cb7c88ff541dfa9c8eb2
CatTag <acatcalledjordan@gmail.com>
2024-01-29 23:12:39 +1100

    unfinished level, spikes and animations don't work.

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -3,18 +3,18 @@ extends Control
 @onready var score = $score
 
 func _process(delta):
-    score.text = "Score: " + str(global.score)
+	score.text = "Score: " + str(global.score)
 
 func _on_retry_pressed():
-    get_tree().paused = true
-    get_tree().change_scene_to_file("res://scenes/levels/level"+str(global.current_level)+".tscn")
-    get_tree().paused = false
+	get_tree().paused = true
+	get_tree().change_scene_to_file("res://scenes/levels/level"+str(global.current_level)+".tscn")
+	get_tree().paused = false
 
 func _on_main_menu_pressed():
-    # get_tree().paused = true
-    get_tree().change_scene_to_file("res://scenes/menu.tscn")
-    # get_tree().paused = fals
+	# get_tree().paused = true
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	# get_tree().paused = fals
 
 func _on_quit_pressed():
-    get_tree().quit()
-    
+	get_tree().quit()
+	
