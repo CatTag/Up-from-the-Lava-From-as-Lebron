f0802e271e9feafd0de3d6ac7d6d274817e299db
CatTag <acatcalledjordan@gmail.com>
2024-01-29 21:25:24 +1100

    duped template

--- scripts/level_logic.gd
+++ scripts/level_logic.gd
@@ -7,37 +7,37 @@ extends Node2D
 
 
 func _on_lava_body_entered(body):
-    if body.name == "player":
-        game_over()
+	if body.name == "player":
+		game_over()
 
 func _on_spike_body_entered(body):
-    if body.name == "player":
-        game_over()
+	if body.name == "player":
+		game_over()
 
 func game_over():
 #    global.score = timer.text
-    death.play(0.0)
-    global.score = timer.get_child(0).text
-    game_over_screen.visible = true
-    get_tree().paused = true
-    timer.visible = false
-    
+	death.play(0.0)
+	global.score = timer.get_child(0).text
+	game_over_screen.visible = true
+	get_tree().paused = true
+	timer.visible = false
+	
 
 func game_win():
-    pass
-    
+	pass
+	
 func restart():
-    get_tree().change_scene_to_file(get_level())
+	get_tree().change_scene_to_file(get_level())
 
 func get_level():
-    return "res://scenes/levels/level" + str(global.current_level) + ".tscn"
+	return "res://scenes/levels/level" + str(global.current_level) + ".tscn"
 
 func _process(delta):
-    if Input.is_action_pressed("restart"):
-        restart()
+	if Input.is_action_pressed("restart"):
+		restart()
 
 func _on_finish_line_body_entered(body):
-    global.score = timer.get_child(0).text
-    game_win_screen.visible = true
-    get_tree().paused = true
-    timer.visible = false
+	global.score = timer.get_child(0).text
+	game_win_screen.visible = true
+	get_tree().paused = true
+	timer.visible = false
